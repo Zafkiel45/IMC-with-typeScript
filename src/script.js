68a47edca6cb9fea99b1e7f63b6929740f5c85ab
Zafkiel45 <zafkielc@gmail.com>
2024-01-04 10:46:20 -0300

    feat: tests

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
-function teste() {
-}
+var myButton = document.querySelector("#button");
+var myInputHeight = document.querySelector("#altura");
+var myInputWeight = document.querySelector("#peso");
+var result = document.querySelector("#result");
+myButton.addEventListener('click', function () {
+    var IMC = Number(myInputWeight.value) / Number(myInputHeight.value);
+    result.style.display = 'block';
+    result.textContent = "SEU IMC \u00C9 - ".concat(IMC.toFixed(2));
+});
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,10 @@
-function teste():void {
-    
-}
+const myButton = document.querySelector("#button") as HTMLButtonElement;
+const myInputHeight = document.querySelector("#altura") as HTMLInputElement;
+const myInputWeight = document.querySelector("#peso") as HTMLInputElement;
+const result = document.querySelector("#result") as HTMLDivElement;
+
+myButton.addEventListener('click', ():void => {
+    const IMC: number = Number(myInputWeight.value) / Number(myInputHeight.value);
+    result.style.display = 'block';
+    result.textContent = `SEU IMC É - ${IMC.toFixed(2)}`
+})
